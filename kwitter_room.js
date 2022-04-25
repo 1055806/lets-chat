3e81c70ac8ed456a78dfd25cb6511c38297e02cf
1055806 <94760194+1055806@users.noreply.github.com>
2022-04-25 19:57:52 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,21 @@
+
+var firebaseConfig = {
+      apiKey: "AIzaSyD0GxhBkuovJsm9PrbaKAWi0n86N2AWx-8",
+      authDomain: "kwitter-a81f3.firebaseapp.com",
+      databaseURL: "https://kwitter-a81f3-default-rtdb.firebaseio.com",
+      projectId: "kwitter-a81f3",
+      storageBucket: "kwitter-a81f3.appspot.com",
+      messagingSenderId: "827263873123",
+      appId: "1:827263873123:web:ec3ede7e8cb3976c4d4856"
+    };
+    
+    // Initialize Firebase
+     firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
